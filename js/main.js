dce3b8f77734740abcd96d78a29ccd9b445e04c9
Durairaj Subramaniam <Durairaj.Subramaniam@gmail.com>
2025-03-31 09:06:52 +0100

    fix: unwanted files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
 $(function() {
 
+    $('.sidebar-section-title').click(function() {
+        console.log('Title clicked:', $(this).text());
+        $(this).toggleClass('collapsed');
+        $(this).next('.sidebar-section-content').slideToggle(200);
+    });
+
+    // Expand active section
+    $('.sidebar-section-content').each(function() {
+        if ($(this).find('li.active').length > 0) {
+            console.log('Expanding section with active item');
+            $(this).slideDown(0);
+            $(this).prev('.sidebar-section-title').removeClass('collapsed');
+        }
+    });
+    
     // $('.collapse').collapse('hide');
     $('.list-group-item.active').parent().parent('.collapse').collapse('show');
 
